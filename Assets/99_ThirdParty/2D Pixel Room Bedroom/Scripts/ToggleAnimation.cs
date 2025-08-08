ad7eb504872f75b6dd96faf810ce1d691fb0089e
leegong <leegong@111percent.net>
2025-08-09 01:15:24 +0900

    chore: 필요 에셋 업로드

---
+++ Assets/99_ThirdParty/2D Pixel Room Bedroom/Scripts/ToggleAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class ToggleAnimation : MonoBehaviour {
+	public Toggle Toggle;
+	private bool ToggleValues;
+    public Animation Animation;
+	public string on = "";
+	public string off = "";
+
+	private void Start() {
+		Toggle.onValueChanged.AddListener((bool tempVar) => {
+			ToggleValues = tempVar;
+			OnToggleClick(Toggle, ToggleValues);
+		});
+	}
+
+	public void OnToggleClick(Toggle toggle, bool value) {
+
+			if(Toggle.isOn) {
+				Animation.Play(on);
+			} else {
+				Animation.Play(off);
+			}
+	}
+}
